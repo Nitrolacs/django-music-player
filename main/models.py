72071d527c69c42928c418c28f30d3b6061a5833
Terentev Nikita <nitro-2003@mail.ru>
2022-10-19 22:28:32 +0700

    Added music file extension check.

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from src.linked_list import LinkedList
+from main.helpers import get_audio_length
+from main.validators import validate_is_audio
 
 
 class Composition(models.Model):
@@ -7,13 +9,14 @@ class Composition(models.Model):
     artist = models.CharField(max_length=500)
     playlist = models.ForeignKey('PlayList', on_delete=models.SET_NULL, null=True, blank=True)
     time_length = models.DecimalField(blank=True, max_digits=20, decimal_places=2)
-    audio_file = models.FileField(upload_to='musics/')
+    audio_file = models.FileField(upload_to='musics/', validators=[validate_is_audio])
     cover_image = models.ImageField(upload_to='music_image/')
 
     def save(self, *args, **kwargs):
         if not self.time_length:
             # логика для получения длины музыкального трека
-            pass
+            audio_length = get_audio_length(self.audio_file)
+            self.time_length = audio_length
         return super().save(*args, **kwargs)
 
 
